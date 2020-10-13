bd81a4e79d9659ff4cd14cf406d0ce8c38c67a13
Kat Scriver <klescriver@gmail.com>
2020-10-13 09:43:42 -0600

    Add test for planet name.

--- test/planet_test.rb
+++ test/planet_test.rb
@@ -6,6 +6,12 @@ class PlanetTest < MiniTest::Test
   def test_it_exists
     planet = Planet.new("Saturn")
 
-    assert_instance_of Planet, planet 
+    assert_instance_of Planet, planet
+  end
+
+  def test_it_has_a_name
+    planet = Planet.new("Saturn")
+
+    assert_equal "Saturn", planet.name
   end
 end
