044dbb62e4ce2a4c9a8ce0538ccc2760dc40afcc
Meghan Stang <mstang15@gmail.com>
2020-10-13 11:32:13 -0400

    Add distance_from_sun attribute on planet

--- test/planet_test.rb
+++ test/planet_test.rb
@@ -4,8 +4,8 @@ require './lib/planet'
 
 class PlanetTest < MiniTest::Test
   def test_it_exists
-    planet = Planet.new("Saturn")
+    planet = Planet.new("Saturn", 12345678)
 
-    assert_instance_of Planet, planet 
+    assert_instance_of Planet, planet
   end
 end
