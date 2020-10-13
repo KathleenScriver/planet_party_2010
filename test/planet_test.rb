4fff15b896fc67895e7a91c3f7b628ddb6da8aa0
Meghan Stang <mstang15@gmail.com>
2020-10-13 11:23:55 -0400

    Add test it exists for planet class

--- test/planet_test.rb
+++ test/planet_test.rb
@@ -3,5 +3,9 @@ require 'minitest/pride'
 require './lib/planet'
 
 class PlanetTest < MiniTest::Test
-  
+  def test_it_exists
+    planet = Planet.new("Saturn")
+
+    assert_instance_of Planet, planet 
+  end
 end
